eb65fc322be15ab091d436d8a4e7d34439517f1f
ndergunov <ndergunov@gmail.com>
2022-03-29 14:10:10 +0300

    delivery service api first commit

--- app/services/delivery/cmd/server/server.go
+++ app/services/delivery/cmd/server/server.go
@@ -1,18 +1,19 @@
 package server
 
 import (
+	"log"
 	"net/http"
 
-	"github.com/nndergunov/deliveryApp/app/libs/logger"
 	"github.com/nndergunov/deliveryApp/app/services/delivery/cmd/server/config"
 )
 
 type Server struct {
 	HTTPServer *http.Server
-	Logger     logger.Logger
 }
 
-func NewServer(handler http.Handler, serverConfig config.Config, logger logger.Logger) *Server {
+func NewServer(handler http.Handler, errorLog *log.Logger) *Server {
+	serverConfig := config.GetConfig()
+
 	return &Server{
 		HTTPServer: &http.Server{
 			Addr:              serverConfig.Address,
@@ -25,11 +26,10 @@ func NewServer(handler http.Handler, serverConfig config.Config, logger logger.L
 			MaxHeaderBytes:    0,
 			TLSNextProto:      nil,
 			ConnState:         nil,
-			ErrorLog:          nil,
+			ErrorLog:          errorLog,
 			BaseContext:       nil,
 			ConnContext:       nil,
 		},
-		Logger: logger,
 	}
 }
 
@@ -41,5 +41,5 @@ func (s *Server) StartListening() {
 }
 
 func (s Server) Log(data any) {
-	s.Logger.Println(data)
+	s.HTTPServer.ErrorLog.Println(data)
 }
