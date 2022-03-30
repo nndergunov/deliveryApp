6e25f74baa9b25e83f47a0495e16224d221b3120
ndergunov <ndergunov@gmail.com>
2022-03-30 12:59:31 +0300

    Delivery service API

--- app/services/delivery/go.mod
+++ app/services/delivery/go.mod
@@ -1,3 +1,5 @@
 module github.com/nndergunov/deliveryApp/app/services/delivery
 
 go 1.18
+
+require github.com/nndergunov/deliveryApp/app/libs/logger v0.0.0-20220329110511-27b6a4494a95
--- app/services/delivery/main.go
+++ app/services/delivery/main.go
@@ -1,21 +1,39 @@
 package main
 
 import (
-	"log"
 	"os"
+	"time"
 
+	"github.com/nndergunov/deliveryApp/app/libs/logger"
 	"github.com/nndergunov/deliveryApp/app/services/delivery/api"
 	"github.com/nndergunov/deliveryApp/app/services/delivery/cmd/server"
+	"github.com/nndergunov/deliveryApp/app/services/delivery/cmd/server/config"
 )
 
 func main() {
-	// apiLogger := logger.NewLogger(os.Stdout, "delivery api")
+	apiLogger := logger.NewLogger(os.Stdout, "delivery api")
 
 	deliveryAPI := api.NewAPI(apiLogger)
 
-	serverLogger := log.New(os.Stdout, "delivery server ", log.LstdFlags)
+	var (
+		address          = ":8080"
+		readTime         = 5 * time.Second
+		writeTime        = 5 * time.Second
+		idleTime         = 300 * time.Second
+		readerHeaderTime = 5 * time.Second
+	) // TODO change this to viper and yaml.
 
-	deliveryServer := server.NewServer(deliveryAPI, serverLogger)
+	serverConfig := config.Config{
+		Address:           address,
+		ReadTimeout:       readTime,
+		WriteTimeout:      writeTime,
+		IdleTimeout:       idleTime,
+		ReadHeaderTimeout: readerHeaderTime,
+	}
+
+	serverLogger := logger.NewLogger(os.Stdout, "delivery server")
+
+	deliveryServer := server.NewServer(deliveryAPI, serverConfig, serverLogger)
 
 	deliveryServer.StartListening()
 }
