72c6e00d024a7b0a8f020a61af740b5c5f81d79d
ndergunov <ndergunov@gmail.com>
2022-04-01 00:13:55 +0300

    delivery api endpoints

--- app/services/delivery/main.go
+++ app/services/delivery/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"time"
 
@@ -8,32 +9,70 @@ import (
 	"github.com/nndergunov/deliveryApp/app/services/delivery/api"
 	"github.com/nndergunov/deliveryApp/app/services/delivery/cmd/server"
 	"github.com/nndergunov/deliveryApp/app/services/delivery/cmd/server/config"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	apiLogger := logger.NewLogger(os.Stdout, "delivery api")
+	mainLogger := logger.NewLogger(os.Stdout, "main")
 
-	deliveryAPI := api.NewAPI(apiLogger)
+	apiLogger := logger.NewLogger(os.Stdout, "delivery api")
+	serverAPI := api.NewAPI(apiLogger)
+
+	serverConfig, err := getServerConfig()
+	if err != nil {
+		mainLogger.Println(err)
+	}
+
+	serverLogger := logger.NewLogger(os.Stdout, "service server")
+	serviceServer := server.NewServer(serverAPI, serverConfig, serverLogger)
+	serverStopChan := make(chan interface{})
+
+	serviceServer.StartListening(serverStopChan)
+
+	err = write("press Enter to stop server\n")
+	if err != nil {
+		mainLogger.Println(err)
+	}
+
+	_, err = fmt.Scanln()
+	if err != nil {
+		mainLogger.Println(err)
+	}
+
+	serviceServer.Shutdown()
+
+	<-serverStopChan
+}
+
+func getServerConfig() (*config.Config, error) {
+	viper.SetConfigFile("config.yaml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("config read: %w", err)
+	}
 
 	var (
-		address          = ":8080"
-		readTime         = 5 * time.Second
-		writeTime        = 5 * time.Second
-		idleTime         = 300 * time.Second
-		readerHeaderTime = 5 * time.Second
-	) // TODO change this to viper and yaml.
+		address          = viper.GetString("server.address")
+		readTime         = time.Duration(viper.GetInt("server.readTime")) * time.Second
+		writeTime        = time.Duration(viper.GetInt("server.writeTime")) * time.Second
+		idleTime         = time.Duration(viper.GetInt("server.idleTime")) * time.Second
+		readerHeaderTime = time.Duration(viper.GetInt("server.readerHeaderTime")) * time.Second
+	)
 
-	serverConfig := config.Config{
+	return &config.Config{
 		Address:           address,
 		ReadTimeout:       readTime,
 		WriteTimeout:      writeTime,
 		IdleTimeout:       idleTime,
 		ReadHeaderTimeout: readerHeaderTime,
+	}, nil
+}
+
+func write(s string) error {
+	_, err := os.Stdout.WriteString(s)
+	if err != nil {
+		return fmt.Errorf("writing to stdout: %w", err)
 	}
 
-	serverLogger := logger.NewLogger(os.Stdout, "delivery server")
-
-	deliveryServer := server.NewServer(deliveryAPI, serverConfig, serverLogger)
-
-	deliveryServer.StartListening()
+	return nil
 }
