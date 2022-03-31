9bc7003ae2ef73aaf27913814771fa2151ddc415
ndergunov <ndergunov@gmail.com>
2022-03-31 21:13:37 +0300

    log wrapper method upgrade

--- app/libs/logger/logger.go
+++ app/libs/logger/logger.go
@@ -17,6 +17,38 @@ func NewLogger(out io.Writer, prefix string) *Logger {
 	}
 }
 
-func (l Logger) Println(data any) {
-	l.logger.Println(data)
+func (l Logger) Printf(format string, data ...any) {
+	l.logger.Printf(format, data...)
+}
+
+func (l Logger) Println(data ...any) {
+	l.logger.Println(data...)
+}
+
+func (l Logger) Print(data ...any) {
+	l.logger.Print(data...)
+}
+
+func (l Logger) Fatal(data ...any) {
+	l.logger.Fatal(data...)
+}
+
+func (l Logger) Fatalf(format string, data ...any) {
+	l.logger.Fatalf(format, data...)
+}
+
+func (l Logger) Fatalln(data ...any) {
+	l.logger.Fatalln(data...)
+}
+
+func (l Logger) Panic(data ...any) {
+	l.logger.Panic(data...)
+}
+
+func (l Logger) Panicf(format string, data ...any) {
+	l.logger.Panicf(format, data...)
+}
+
+func (l Logger) Panicln(data ...any) {
+	l.logger.Panicln(data...)
 }
