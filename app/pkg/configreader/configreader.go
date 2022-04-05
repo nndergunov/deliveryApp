37c022dc41f5368fa7deb3e15c4e492ca6730ccb
ndergunov <ndergunov@gmail.com>
2022-04-05 12:23:23 +0300

    viper wrapper

---
+++ app/pkg/configreader/configreader.go
@@ -0,0 +1,34 @@
+package configreader
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+// SetConfigFile defines path and name of the desired config file.
+func SetConfigFile(path string) error {
+	viper.SetConfigFile("config.yaml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return fmt.Errorf("config read: %w", err)
+	}
+
+	return nil
+}
+
+// GetString reads string with the specified key from the config file declared in SetConfigFile.
+func GetString(key string) string {
+	return viper.GetString(key)
+}
+
+// GetInt reads int with the specified key from the config file declared in SetConfigFile.
+func GetInt(key string) int {
+	return viper.GetInt(key)
+}
+
+// GetDuration reads time.Duration with the specified key from the config file declared in SetConfigFile.
+func GetDuration(key string) time.Duration {
+	return viper.GetDuration(key)
+}
