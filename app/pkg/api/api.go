74e429c2dc9c9ba3282cec29f2a577fb5c3cd6a1
ndergunov <ndergunov@gmail.com>
2022-05-03 15:37:26 +0300

    removed gorilla/mux dependency from api

--- app/pkg/api/api.go
+++ app/pkg/api/api.go
@@ -1,21 +1,25 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
-
+	v1 "github.com/nndergunov/deliveryApp/app/pkg/api/v1"
 	"github.com/nndergunov/deliveryApp/app/pkg/logger"
 )
 
+type Multiplexer interface {
+	ServeHTTP(w http.ResponseWriter, r *http.Request)
+}
+
 // API is main server handler.
 type API struct {
-	serveMux *mux.Router
+	serveMux Multiplexer
 	log      *logger.Logger
 }
 
 // NewAPI returns new instance of api.API.
-func NewAPI(endpointHandler *mux.Router, log *logger.Logger) *API {
+func NewAPI(endpointHandler Multiplexer, log *logger.Logger) *API {
 	api := &API{
 		serveMux: endpointHandler,
 		log:      log,
@@ -27,4 +31,24 @@ func NewAPI(endpointHandler *mux.Router, log *logger.Logger) *API {
 // ServeHTTP method satisfies http.Handler interface.
 func (a *API) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	a.serveMux.ServeHTTP(w, r)
+}
+
+func Respond(response any, w http.ResponseWriter) error {
+	data, err := v1.Encode(response)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return fmt.Errorf("api.Respond: %w", err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	_, err = w.Write(data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return fmt.Errorf("api.Respond: %w", err)
+	}
+
+	return nil
 }
