fab389178854f809b679d82940adb6a97c607cad
Shohrukh Kholov <shohrukh_kholov@epam.com>
2022-06-22 17:39:39 +0300

    fully tested and integrated

--- app/services/delivery/pkg/clients/restaurantclient/restaurantclient.go
+++ app/services/delivery/pkg/clients/restaurantclient/restaurantclient.go
@@ -1,7 +1,11 @@
 package restaurantclient
 
 import (
+	"fmt"
+	"github.com/nndergunov/deliveryApp/app/pkg/api/v1/deliveryapi"
 	"github.com/nndergunov/deliveryApp/app/pkg/api/v1/restaurantapi"
+	"net/http"
+	"strconv"
 )
 
 type RestaurantClient struct {
@@ -13,32 +17,23 @@ func NewRestaurantClient(url string) *RestaurantClient {
 }
 
 func (a RestaurantClient) GetRestaurant(restaurantID int) (*restaurantapi.ReturnRestaurant, error) {
-	//resp, err := http.Get(a.restaurantURL + "v1/restaurants/" + strconv.Itoa(restaurantID))
-	//if err != nil {
-	//	return nil, fmt.Errorf("sending request: %w", err)
-	//}
-	//
-	//if resp.StatusCode != http.StatusOK {
-	//	return nil, fmt.Errorf("not ok status: %v", resp.StatusCode)
-	//}
-	//
-	//restaurantData := restaurantapi.ReturnRestaurant{}
-	//if err = deliveryapi.DecodeJSON(resp.Body, &restaurantData); err != nil {
-	//	return nil, fmt.Errorf("decoding : %w", err)
-	//}
-	//
-	//if err := resp.Body.Close(); err != nil {
-	//	return nil, err
-	//}
-
-	restaurantData := restaurantapi.ReturnRestaurant{
-		ID:              1,
-		Name:            "Balboo",
-		AcceptingOrders: true,
-		City:            "Istanbul",
-		Address:         "Bagcilar",
-		Longitude:       28.860093289472598,
-		Latitude:        41.036234457409066,
+	resp, err := http.Get(a.restaurantURL + "v1/restaurants/" + strconv.Itoa(restaurantID))
+	if err != nil {
+		return nil, fmt.Errorf("sending request: %w", err)
 	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("not ok status: %v", resp.StatusCode)
+	}
+
+	restaurantData := restaurantapi.ReturnRestaurant{}
+	if err = deliveryapi.DecodeJSON(resp.Body, &restaurantData); err != nil {
+		return nil, fmt.Errorf("decoding : %w", err)
+	}
+
+	if err := resp.Body.Close(); err != nil {
+		return nil, err
+	}
+
 	return &restaurantData, nil
 }
