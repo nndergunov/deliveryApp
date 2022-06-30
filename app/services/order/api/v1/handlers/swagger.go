098622dda37bd0405712ba3bd1eb61235bb09ebd
ndergunov <ndergunov@gmail.com>
2022-06-30 16:51:53 +0300

    order service basic swagger doc

---
+++ app/services/order/api/v1/handlers/swagger.go
@@ -0,0 +1,15 @@
+// API specification of the order service
+//
+//     Schemes: http
+//     Host: localhost
+//     BasePath: /v1
+//     Version: 0.0.1
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+// swagger:meta
+package handlers
