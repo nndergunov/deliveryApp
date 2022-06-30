7bf21a2970c1949613d3e14d4650a181aa9899ba
ndergunov <ndergunov@gmail.com>
2022-06-30 17:50:29 +0300

    kitchen service basic swagger

---
+++ app/services/kitchen/api/v1/handlers/swagger.go
@@ -0,0 +1,15 @@
+// API specification of the kitchen service
+//
+//     Schemes: http
+//     Host: localhost
+//     BasePath: /v1
+//     Version: 0.0.1
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+// swagger:meta
+package handlers
