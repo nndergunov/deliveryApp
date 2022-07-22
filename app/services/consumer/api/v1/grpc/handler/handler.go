bccbe4c0b362da1560ee7fd9b3bbe5d60f0c76e2
Shohrukh Kholov <shohrukh_kholov@epam.com>
2022-07-22 12:54:13 +0300

    grpc:consumer

--- app/services/consumer/api/v1/grpc/handler/handler.go
+++ app/services/consumer/api/v1/grpc/handler/handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"context"
 	"github.com/nndergunov/deliveryApp/app/pkg/logger"
+	"strconv"
 
 	"github.com/nndergunov/deliveryApp/app/services/consumer/pkg/domain"
 
@@ -78,32 +79,135 @@ func (h *handler) GetAllConsumer(ctx context.Context, in *pb.SearchParam) (*pb.C
 	return &pb.ConsumerListResponse{ConsumerListResponse: outList}, nil
 }
 
-func (h *handler) DeleteConsumer(ctx context.Context, id *pb.ConsumerID) (*pb.ConsumerDeleteResponse, error) {
-	//TODO implement me
-	panic("implement me")
+func (h *handler) DeleteConsumer(ctx context.Context, in *pb.ConsumerID) (*pb.ConsumerDeleteResponse, error) {
+	out, err := h.service.DeleteConsumer(strconv.Itoa(int(in.ConsumerID)))
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.ConsumerDeleteResponse{ConsumerDeleteResponse: out}, nil
 }
 
-func (h *handler) UpdateConsumer(ctx context.Context, request *pb.UpdateConsumerRequest) (*pb.ConsumerResponse, error) {
-	//TODO implement me
-	panic("implement me")
+func (h *handler) UpdateConsumer(ctx context.Context, in *pb.UpdateConsumerRequest) (*pb.ConsumerResponse, error) {
+	consumer := domain.Consumer{
+		Firstname: in.Firstname,
+		Lastname:  in.Lastname,
+		Email:     in.Email,
+		Phone:     in.Phone,
+	}
+
+	resp, err := h.service.UpdateConsumer(consumer, strconv.FormatInt(in.ID, 10))
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.ConsumerResponse{
+		ID:        int64(resp.ID),
+		Firstname: resp.Firstname,
+		Lastname:  resp.Lastname,
+		Email:     resp.Email,
+		Phone:     resp.Phone,
+	}, nil
 }
 
-func (h *handler) GetConsumer(ctx context.Context, id *pb.ConsumerID) (*pb.ConsumerResponse, error) {
-	//TODO implement me
-	panic("implement me")
+func (h *handler) GetConsumer(ctx context.Context, in *pb.ConsumerID) (*pb.ConsumerResponse, error) {
+	resp, err := h.service.GetConsumer(strconv.FormatInt(in.ConsumerID, 10))
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.ConsumerResponse{
+		ID:        int64(resp.ID),
+		Firstname: resp.Firstname,
+		Lastname:  resp.Lastname,
+		Email:     resp.Email,
+		Phone:     resp.Phone,
+	}, nil
 }
 
-func (h *handler) InsertNewConsumerLocation(ctx context.Context, location *pb.Location) (*pb.Location, error) {
-	//TODO implement me
-	panic("implement me")
+func (h *handler) InsertNewConsumerLocation(ctx context.Context, in *pb.Location) (*pb.Location, error) {
+	location := domain.Location{
+		UserID:     int(in.UserID),
+		Latitude:   *in.Latitude,
+		Longitude:  *in.Longitude,
+		Country:    *in.Country,
+		City:       *in.City,
+		Region:     *in.Region,
+		Street:     *in.Street,
+		HomeNumber: *in.HomeNumber,
+		Floor:      *in.Floor,
+		Door:       *in.Door,
+	}
+
+	resp, err := h.service.InsertLocation(location, strconv.Itoa(location.UserID))
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Location{
+		UserID:     int64(resp.UserID),
+		Latitude:   &resp.Latitude,
+		Longitude:  &resp.Longitude,
+		Country:    &resp.Country,
+		City:       &resp.City,
+		Region:     &resp.Region,
+		Street:     &resp.Street,
+		HomeNumber: &resp.HomeNumber,
+		Floor:      &resp.Floor,
+		Door:       &resp.Door,
+	}, nil
 }
 
-func (h *handler) UpdateConsumerLocation(ctx context.Context, location *pb.Location) (*pb.Location, error) {
-	//TODO implement me
-	panic("implement me")
+func (h *handler) UpdateConsumerLocation(ctx context.Context, in *pb.Location) (*pb.Location, error) {
+	location := domain.Location{
+		UserID:     int(in.UserID),
+		Latitude:   *in.Latitude,
+		Longitude:  *in.Longitude,
+		Country:    *in.Country,
+		City:       *in.City,
+		Region:     *in.Region,
+		Street:     *in.Street,
+		HomeNumber: *in.HomeNumber,
+		Floor:      *in.Floor,
+		Door:       *in.Door,
+	}
+
+	resp, err := h.service.UpdateLocation(location, strconv.Itoa(location.UserID)))
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Location{
+		UserID:     int64(resp.UserID),
+		Latitude:   &resp.Latitude,
+		Longitude:  &resp.Longitude,
+		Country:    &resp.Country,
+		City:       &resp.City,
+		Region:     &resp.Region,
+		Street:     &resp.Street,
+		HomeNumber: &resp.HomeNumber,
+		Floor:      &resp.Floor,
+		Door:       &resp.Door,
+	}, nil
 }
 
-func (h *handler) GetConsumerLocation(ctx context.Context, id *pb.UserID) (*pb.Location, error) {
-	//TODO implement me
-	panic("implement me")
+func (h *handler) GetConsumerLocation(ctx context.Context, in *pb.UserID) (*pb.Location, error) {
+
+	resp, err := h.service.GetLocation(strconv.FormatInt(in.UserID, 10))
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Location{
+		UserID:     int64(resp.UserID),
+		Latitude:   &resp.Latitude,
+		Longitude:  &resp.Longitude,
+		Country:    &resp.Country,
+		City:       &resp.City,
+		Region:     &resp.Region,
+		Street:     &resp.Street,
+		HomeNumber: &resp.HomeNumber,
+		Floor:      &resp.Floor,
+		Door:       &resp.Door,
+	}, nil
 }
